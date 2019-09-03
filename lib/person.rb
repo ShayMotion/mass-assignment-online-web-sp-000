e62731d8e01cee26ea8127753274a159d2f50551
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-09-03 03:16:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,11 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+                :weight, :handed, :complexion, :t_shirt_size,
+                :wrist_size, :glove_size, :pant_length, :pant_width
 
-def initialize
-  hash = [:name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width]
-end
-
-sophie = Person.new
-sophie.send("name: "Sophie", hair_color: "Brown")
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send(("#{key}="), value)
+    end
+  end
 end
